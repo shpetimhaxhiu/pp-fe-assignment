7aa58ba72868118e216c31262d89449bf23c0249
Shpetim Haxhiu <shpetim.h@gmail.com>
2022-05-22 03:51:39 +0200

    Added axios and tried data from github

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
 <div>
   <Navbar></Navbar>
-  
+  <div 
+  v-if="repositories.length > 0">
+  <div class="row px-2">
+  <UserCard  v-for="repo in repositories" :key="repo.id" :avatar="repo.avatar_url" :url="repo.url" :username="repo.login"></UserCard>
+    
+  </div>
+  </div>
+
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Navbar from './components/shared/Navbar.vue';
+import UserCard from './components/UserCard.vue';
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    UserCard
   },
+  data(){
+    return {
+      repositories: [],
+      errors: []
+    }
+  },
+
+  async created(){
+    try {
+      const response = await axios.get(`https://api.github.com/users?page=1&per_page=10`)
+      this.repositories = response.data
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
 };
 </script>
 
--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <div class="card p-2 m-2"  style="width: 18rem;">
+  <img :src="avatar" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">{{username}}</h5>
+    <p class="card-text">{{otherData}}</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
@@ -11,8 +11,32 @@
 
 <script>
 export default {
-  name: "UserCard"
-};
+  name: "UserCard",
+  props: {
+ username : {
+      type: String,
+      required: true,
+      default: ''
+    },
+    avatar: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    url : {
+      type: String,
+      required: true,
+      default: ''
+    },
+    otherData : {
+      type: Object,
+      required: false,
+      default: {}
+    }
+  }
+   
+  }
+;
 </script>
 
 <style>
